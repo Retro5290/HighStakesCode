f5a5312b0ff0061e2de0052fe720e5abfae3924d
RandomPerJerry <128206975+RandomPerJerry@users.noreply.github.com>
2024-11-02 17:15:42 -0700

    Comp 1 Legacy

--- src/main.cpp
+++ src/main.cpp
@@ -7,12 +7,12 @@ pros::Controller controller(pros::E_CONTROLLER_MASTER);
 pros::MotorGroup left_motors({-16, -18, -17}, pros::MotorGearset::blue); // left motors on ports 1, 2, 3
 pros::MotorGroup right_motors({4, 3, 1}, pros::MotorGearset::blue); // right motors on ports 4, 5, 6
 
-pros::Motor ramp_motor1(11, pros::MotorGearset::blue); // ramp motor on port D
-pros::Motor ramp_motor2(12, pros::MotorGearset::blue); // ramp motor on port D
+pros::MotorGroup intakeMotors({11, -12}, pros::MotorGearset::blue);
 
-pros::ADIDigitalOut clampPiston('A'); // clamp on port E
-pros::ADIDigitalOut intakePiston('B'); // intake on port F
-pros::ADIDigitalOut dongerPiston('C'); // intake on port G
+pros::ADIDigitalOut clampPiston('A');
+pros::ADIDigitalOut intakePiston('B');
+pros::ADIDigitalOut dongerPiston('C'); 
+pros::ADIDigitalOut tankSwap('D');
 
 //Drive train
 lemlib::Drivetrain drivetrain(
@@ -24,14 +24,14 @@ lemlib::Drivetrain drivetrain(
     2 // horizontal drift is 2 (for now)
 );
 
-pros::adi::Encoder vertical_encoder('D', 'E'); // left encoder on ports 1, 2 Relative to the tracking center: front is positive, back is negative
-pros::Imu imu(5);
+pros::adi::Encoder vertical_encoder('E', 'F'); // left encoder on ports 1, 2 Relative to the tracking center: front is positive, back is negative
+pros::Imu imu(20);
 
 lemlib::TrackingWheel vertical_tracking_wheel(
 	&vertical_encoder, // encoder
 	lemlib::Omniwheel::NEW_275, 
 	0.0 // 0 inch distance from tracking center
-);
+); 
 
 lemlib::OdomSensors sensors(&vertical_tracking_wheel,
                             nullptr,
@@ -79,6 +79,8 @@ lemlib::Chassis chassis(drivetrain, // drivetrain settings
 void initialize() {
     pros::lcd::initialize(); // initialize brain screen
     chassis.calibrate(); // calibrate sensors
+    chassis.setBrakeMode(pros::E_MOTOR_BRAKE_HOLD);
+
     // print position to brain screen
     pros::Task screen_task([&]() {
         while (true) {
@@ -86,7 +88,6 @@ void initialize() {
             pros::lcd::print(0, "X: %f", chassis.getPose().x); // x
             pros::lcd::print(1, "Y: %f", chassis.getPose().y); // y
             pros::lcd::print(2, "Theta: %f", chassis.getPose().theta); // heading
-            pros::lcd::print(3, "Intake: %d", ramp_motor1.get_target_velocity());
             // delay to save resources
             pros::delay(20);
         }
@@ -123,10 +124,57 @@ void competition_initialize() {}
  * from where it left off.
  */
 
-ASSET(lap1_txt)
+ASSET(toMogol1test_txt)
 void autonomous() {
-    chassis.setPose(47.482, -46.707, 0); // set robot position to (0, 0, 0)
-    chassis.follow(lap1_txt, 10, 20000); // follow the path
+    //2
+    chassis.setPose(-57.526, 46.289, 305); 
+    clampPiston.set_value(true);
+    pros::delay(10);
+    chassis.moveToPoint(-32.665, 28.701, 4000, {.forwards = false, .maxSpeed = 40});
+    chassis.waitUntilDone();
+    clampPiston.set_value(false);
+    pros::delay(500);
+    chassis.turnToPoint(-22.977, 49.596, 4000);
+    chassis.waitUntilDone();
+    intakeMotors.move_velocity(600);
+    chassis.moveToPoint(-22.977, 49.596, 4000);
+    chassis.waitUntilDone();
+    intakeMotors.move_velocity(0);
+
+    chassis.turnToHeading(180, 4000);
+    chassis.waitUntilDone();
+   
+    
+    chassis.moveToPoint(-23.412, 9.823, 4000, {.maxSpeed = 50});
+    chassis.waitUntilDone();
+
+    //1
+    // chassis.setPose(57.526, 46.289, 55); 
+    // clampPiston.set_value(true);
+    // pros::delay(10);
+    // chassis.moveToPoint(32.665, 28.701, 4000, {.forwards = false, .maxSpeed = 40});
+    // chassis.waitUntilDone();
+    // clampPiston.set_value(false);
+    // pros::delay(500);
+    // chassis.turnToPoint(22.977, 49.596, 4000);
+    // chassis.waitUntilDone();
+    // intakeMotors.move_velocity(600);
+    // chassis.moveToPoint(22.977, 49.596, 4000);
+
+   
+    // chassis.waitUntilDone();
+    // intakeMotors.move_velocity(0);
+    
+    // chassis.turnToHeading(180, 4000);
+
+
+    // chassis.waitUntilDone();
+
+    // chassis.moveToPoint(23.412, 9.823, 4000, {.maxSpeed = 50});
+    // chassis.waitUntilDone();
+
+
+
 }
 
 /**
@@ -146,52 +194,51 @@ void autonomous() {
 
 void intake() {
     static bool r2_toggle = false;
-    static bool clamp_state = false;
 
     if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_R2)) {
         r2_toggle = !r2_toggle;
     }
 
     if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_R1)) {
-        ramp_motor1.move_velocity(600);
-        ramp_motor2.move_velocity(-600);
+        intakeMotors.move_velocity(600);
     } else if (controller.get_digital(pros::E_CONTROLLER_DIGITAL_L1)) {
-        ramp_motor1.move_velocity(-600);
-        ramp_motor2.move_velocity(600);
+        intakeMotors.move_velocity(-600);
     } else if (r2_toggle) {
-        ramp_motor1.move_velocity(600); 
-        ramp_motor2.move_velocity(-600);
+        intakeMotors.move_velocity(600);
     } else {
-        ramp_motor1.move_velocity(0);
-        ramp_motor2.move_velocity(0);
+        intakeMotors.move_velocity(0);
     }
-
-    if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_RIGHT)) {
-        clamp_state = !clamp_state;
-    }
-    intakePiston.set_value(clamp_state);
 }
 
 void doungler_control(){
     static bool donger_state = false;
-    if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_UP)) {
+    if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_DOWN)) {
         donger_state = !donger_state;
     }
     dongerPiston.set_value(donger_state);
 }
 
-void clamp_control() {
-    static bool clamp_state = true;
+void tank_swap(){
+    static bool tank_state = false;
+    if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_X)) {
+        tank_state = !tank_state;
+    }
+    tankSwap.set_value(tank_state);
+}
+
+void clamp_control() { 
+    static bool clamp_state = false;
     
     if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_B)) {
         clamp_state = !clamp_state;
+        clampPiston.set_value(clamp_state);
     }
-    clampPiston.set_value(clamp_state);
+    
 }
 
 void drive_control(bool &isReversed){
     int leftY = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
-        int rightX = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
+    int rightX = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
 
         if (controller.get_digital_new_press(pros::E_CONTROLLER_DIGITAL_Y)) {
             isReversed = !isReversed;
@@ -207,8 +254,9 @@ void opcontrol() {
     // loop forever
     while (true) {
         
-        // drive_control(reverse_drive);
+        drive_control(reverse_drive);
         clamp_control();
+        // tank_swap();  
         intake(); 
         doungler_control();
 
